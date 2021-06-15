65ddf7509b795f8d0a62c3b520fb04eb8fd01f6c
Farai Mathemera <mathemerafarai@gmail.com>
2021-06-15 19:54:09 +0200

    Add comments for molecule classes

--- Carbon.java
+++ Carbon.java
@@ -2,7 +2,7 @@ package molecule;
 
 public class Carbon extends Thread {
 	
-	private static int carbonCounter =0;
+	private static int carbonCounter = 0;
 	private int id;
 	private Propane sharedPropane;
 	public static boolean alreadyExecuted = false;
@@ -15,23 +15,36 @@ public class Carbon extends Thread {
 	
 	public void run() {
 		try {	 
-
+						/**
+						 *  1. Acquire mutex semaphore. And ensure its run once.
+						 */
 						sharedPropane.mutex.acquire(); 
 							if(!alreadyExecuted) {							
 							sharedPropane.carbonQ.release(3);
 							sharedPropane.hydrogensQ.release(8);
 							alreadyExecuted = true;
 						}
-						
+						/**
+						 * 1. Release mutex semaphore.
+						 * 2. Acquire the hydrogenQ.
+						 * 3. Ensure only 8 hydrogens and 3 carbons reach point.
+						 * 4. Acquire mutex semaphore.
+						 */
 						sharedPropane.mutex.release();
 						sharedPropane.carbonQ.acquire();
 						sharedPropane.barrier.phase1();
 						sharedPropane.mutex.acquire();
-						
+
 						if (sharedPropane.getCarbon() == 0 && sharedPropane.getHydrogen() == 0){
 							System.out.println("---Group Ready for bonding---");
 						}
-							
+						/**
+						 * 1. Add Carbonn molecule.
+						 * 2. Bond Carbon molecule.
+						 * 3. Release mutex semaphore.
+						 * 4. Allow formed ATOM to pass barrier. Reset barrier.
+						 * 5. Release the carbonQ to all new threads
+						 */
 						sharedPropane.addCarbon();		
 						sharedPropane.bond("C"+this.id);
 						sharedPropane.mutex.release();
--- Hydrogen.java
+++ Hydrogen.java
@@ -1,33 +1,42 @@
 package molecule;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Hydrogen extends Thread {
 
-	private static int hydrogenCounter =0;
+	private static int hydrogenCounter = 0;
 	private int id;
 	private Propane sharedPropane;
-	public static boolean flag = true;
-
-	
 	
 	public Hydrogen(Propane propane_obj) {
 		Hydrogen.hydrogenCounter+=1;
 		id=hydrogenCounter;
 		this.sharedPropane = propane_obj;
-		
 	}
 	
 	public void run() {
-		try {	 
+		try {	
+			/**
+			 * 1. Acquire mutex semaphore.
+			 * 2. Release mutex semaphore.
+			 * 3. Acquire the hydrogenQ.
+			 * 4. Ensure only 8 hydrogens and 3 carbons reach point.
+			 * 5. Acquire mutex semaphore.
+			 */
 			sharedPropane.mutex.acquire();
 			sharedPropane.mutex.release();
 			sharedPropane.hydrogensQ.acquire();
 			sharedPropane.barrier.phase1();
 			sharedPropane.mutex.acquire();
-			System.out.println("Hy__Carbon: " + sharedPropane.getCarbon() +"--- Hydrogen: "+sharedPropane.getHydrogen());	
+	
 			if (sharedPropane.getCarbon() == 0 && sharedPropane.getHydrogen() == 0){
 				System.out.println("---Group Ready for bonding---");
 			}
+			/**
+			 * 1. Add hydrogen molecule.
+			 * 2. Bond Hydrogen molecule.
+			 * 3. Release mutex semaphore.
+			 * 4. Allow formed ATOM to pass barrier. Reset barrier.
+			 * 5. Release the hydrogenQ to allow new threads
+			 */
 			sharedPropane.addHydrogen();
 			sharedPropane.bond("H"+this.id);
 			sharedPropane.mutex.release();
